39696299e8d2c762c356d67d6ae503a5d8a3bb77
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2024-10-16 13:54:34 +0500

    Task0

--- Tyuiu.DanilovAS.Sprint6.Task0.V27.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint6.Task0.V27.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.DanilovAS.Sprint6.Task0.V27.Lib
     {
         public double Calculate(int x)
         {
-            double res = Math.Pow(-x,3) + 4 * Math.Pow(x,2) - x*(3/2);
+            double res = Math.Pow(-x,3) + 4 * Math.Pow(x,2) - (3*x/2);
             return Math.Round(res,3);
         }
     }
